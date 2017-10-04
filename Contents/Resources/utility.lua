34b1305fef675e911df9bb679e228e82f9de1e97
Federico Bonelli <fredd@dyne.org>
2017-10-04 09:00:16 +0200

    updated utility

--- Contents/Resources/utility.lua
+++ Contents/Resources/utility.lua
@@ -2,6 +2,7 @@
 -- a function that takes a value in an interval and maps it into a different one
 
 function autoscale(n,old_top,old_bottom,new_top,new_bottom)
+
 -- =IF((F30-E30)>0,ROUNDUP(((D30-E30)/(F30-E30))*(H30-G30)+G30,6),0)
 -- example autoscale (0.234, 1, -1, 0, 1)
 -- control: C	temperatura	22	-15	 50	 0	  1	  0.569231
@@ -9,8 +10,11 @@ function autoscale(n,old_top,old_bottom,new_top,new_bottom)
 
 det = (old_top - old_bottom)
 if (det ~= 0) then -- evitare divisioni per zero
-new_value = (n - old_bottom) / (old_top - old_bottom) * (new_top - new_bottom) + new_bottom;
-else return ("division by zero is not allowed!")
+  new_value = (n - old_bottom) / (old_top - old_bottom) * (new_top - new_bottom) + new_bottom;
+  else
+    return ("division by zero is not allowed!")
 end
 return (new_value)
 end
+
+autoscale(22,-15,50,0,1)
