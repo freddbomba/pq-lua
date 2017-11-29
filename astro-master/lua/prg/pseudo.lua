b8158dad6f2c4a70e84cc9136f3ffaee60432cc6
Federico Bonelli <fredd@dyne.org>
2017-11-29 23:05:10 +0100

    added some libs

---
+++ astro-master/lua/prg/pseudo.lua
@@ -0,0 +1,14 @@
+require "astro.calendar"
+require "astro.pseudoscience"
+
+local cal_to_jd = astro.calendar.cal_to_jd
+local biorhythm = astro.pseudoscience.biorhythm
+local astrology = astro.pseudoscience.astrology
+
+print(biorhythm(cal_to_jd(1973, 4, 28.75)))
+
+local bodies = { "Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" }
+for i, p in ipairs(bodies) do
+	local sign = astrology(p, cal_to_jd(1973, 3, 28.75))
+	print(p.." is in "..sign)
+end
--- torrefaro/parserMeteo.lua
+++ torrefaro/parserMeteo.lua
@@ -1,8 +1,14 @@
 require "io"
 io.input(daily.txt)
 while true do
-	local time, n1, n2, n3. n4, n5 = io.read("*number", "*number", "*number", "*number", "*number", "*number")
-	if not n1 then breack end
+	local time, WindDir, WindSpd, WindGust, Humidity, Temp, TotRain, UV, SolarWind, DewPoint, BaromSL, DailyRain, HourRain= io.read("*time", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number", "*number")
+	if not n1 then breack
+	end
 	-- do something with data
 	print(math.max(n1, n2, n3))
 end
+-- Example data
+--Time         Wind Dir   Wind Spd  Wind Gust   Humidity       Temp   Tot Rain         UV      Solar Wind Chill Heat Index  Dew Point   Barom SL  DailyRain   HourRain   RainRate
+-- hr:mm	°      km/hr      km/hr          %         °C         mm                 W/sqm         °C         °C         °C        hPa         mm         mm      mm/hr
+
+-- 18:30             238       12.9       12.9         76       16.4      547.0        0.0          0       16.1       18.5       12.2     1020.8        0.0        0.0       0.00
